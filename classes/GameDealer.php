bf288c9716d2c9735a401083339e55f85cf4f02c
Simon Rininsland <simon.rininsland@aoe.com>
2018-02-22 21:05:44 +0100

    Changing git ignore

---
+++ classes/GameDealer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Simon
+ * Date: 22.02.2018
+ * Time: 14:43
+ */
---
+++ classes/Init.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Simon
+ * Date: 22.02.2018
+ * Time: 10:53
+ */
